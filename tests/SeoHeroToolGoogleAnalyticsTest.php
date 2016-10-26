32c8640e90aa9b198e3060f380fd5f1bb0adc83d
Armin Eden <fritsch.armin@gmail.com>
2016-10-26 11:45:22 +0200

    travis fixing

--- tests/SeoHeroToolGoogleAnalyticsTest.php
+++ tests/SeoHeroToolGoogleAnalyticsTest.php
@@ -25,8 +25,8 @@ class SeoHeroToolGoogleAnalyticsTest extends FunctionalTest
         // Test if  both settings are dev
         $response = $this->get($this->objFromFixture('Page', 'home')->Link());
         $body = strpos($response->getBody(), $this->searchAnalytics);
-        var_dump($response->getBody());
-        var_dump($body);
+        print_r($body);
+//        var_dump($response->getBody());
         //debug::show($response);
         $this->assertTrue(is_numeric($body), _t('SeoHeroToolGoogleAnalyticsTest.CantFindInTemplate').vsprintf(_t('SeoHeroToolGoogleAnalyticsTest.ModeTestedMode'), array('dev', 'dev')));
 
