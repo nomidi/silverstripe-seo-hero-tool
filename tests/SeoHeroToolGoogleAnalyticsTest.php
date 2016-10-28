082986092f84e64c5be2cb5dfa95e90f79dbf3db
Armin Eden <fritsch.armin@gmail.com>
2016-10-28 15:45:52 +0200

    cleanup

--- tests/SeoHeroToolGoogleAnalyticsTest.php
+++ tests/SeoHeroToolGoogleAnalyticsTest.php
@@ -4,7 +4,7 @@ class SeoHeroToolGoogleAnalyticsTest extends FunctionalTest
     protected static $fixture_file = 'SeoHeroToolGoogleAnalyticsTest.yml';
     public static $use_draft_site = true;
     private $googleAnalyticsKey = 'UA-12345678-1';
-    private $searchAnalytics = 'TEST123';
+    private $searchAnalytics = 'www.google-analytics.com';
 
 
 
@@ -26,7 +26,7 @@ class SeoHeroToolGoogleAnalyticsTest extends FunctionalTest
         $ga = $this->objFromFixture('SeoHeroToolGoogleAnalytics', 'default');
         $response = $this->get($this->objFromFixture('Page', 'home')->Link());
         $body = strpos($response->getBody(), $this->searchAnalytics);
-        $this->assertTrue(is_numeric($body), 'test');
+        $this->assertTrue(is_numeric($body), _t('SeoHeroToolGoogleAnalytics.CantFindInTemplate').vsprintf(_t('SeoHeroToolGoogleAnalyticsTest.ModeTestedMode'), array('dev', 'dev')));
 
         // test if it is not displayed if env is dev but display type set to live
 
