f469eaf7fafdf4585a124763f38bf284bf045cc9
ArminEden <eden@bit-basti.de>
2017-05-16 11:11:02 +0200

    added further tests

--- tests/SeoHeroToolDataObjectTest.php
+++ tests/SeoHeroToolDataObjectTest.php
@@ -64,25 +64,43 @@ class SeoHeroToolDataObjectTest extends FunctionalTest
     public function testFBspecificTitle()
     {
         $obj = $this->objFromFixture('Page', 'dataobjecttest');
-        $response = $this->get($this->objFromFixture('Page', 'dataobjecttest')->Link());
+        $response = $this->get($obj->Link());
         $needle = '<meta property="og:title" content="'.$obj->FBTitle.'" />';
         $body = strpos($response->getBody(), $needle);
-        $this->assertTrue(is_numeric($body), 'Could not find a specific FBTitle within the data');
+        $this->assertTrue(is_numeric($body), 'Could not find a specific Facebook Title within the data');
+    }
+
+    public function testFBspecificDescription()
+    {
+        $obj = $this->objFromFixture('Page', 'dataobjecttest');
+        $response = $this->get($obj->Link());
+        $needle = '<meta property="og:description" content="'.$obj->FBDescription.'"';
+        $body = strpos($response->getBody(), $needle);
+        $this->assertTrue(is_numeric($body), 'Count not find a specific Facebook MetaDescription');
     }
 
     public function testTWspecificTitle()
     {
         $obj = $this->objFromFixture('Page', 'dataobjecttest');
-        $response = $this->get($this->objFromFixture('Page', 'dataobjecttest')->Link());
+        $response = $this->get($obj->Link());
         $needle = '<meta name="twitter:title" content="'.$obj->TwTitle.'">';
         $body = strpos($response->getBody(), $needle);
-        $this->assertTrue(is_numeric($body), 'Could not find a specific TwTitle within the data');
+        $this->assertTrue(is_numeric($body), 'Could not find a specific Twitter Title within the data');
+    }
+
+    public function testTWspecificDescription()
+    {
+        $obj = $this->objFromFixture('Page', 'dataobjecttest');
+        $response = $this->get($obj->Link());
+        $needle = '<meta name="twitter:description" content="'.$obj->TwDescription.'">';
+        $body = strpos($response->getBody(), $needle);
+        $this->assertTrue(is_numeric($body), 'Counlt not find a speific Twitter MetaDescription');
     }
 
     public function testFBTitleDefaultTitle()
     {
         $obj = $this->objFromFixture('Page', 'objectWithBetterSiteTitle');
-        $response = $this->get($this->objFromFixture('Page', 'objectWithBetterSiteTitle')->Link());
+        $response = $this->get($obj->Link());
         $needle = '<meta property="og:title" content="'.$obj->BetterSiteTitle.'" />';
         $body = strpos($response->getBody(), $needle);
         $this->assertTrue(is_numeric($body), 'Could not find BetterSiteTitle as og:title ');
@@ -91,9 +109,27 @@ class SeoHeroToolDataObjectTest extends FunctionalTest
     public function testTWTitleDefaultTitle()
     {
         $obj = $this->objFromFixture('Page', 'objectWithBetterSiteTitle');
-        $response = $this->get($this->objFromFixture('Page', 'objectWithBetterSiteTitle')->Link());
+        $response = $this->get($obj->Link());
         $needle = '<meta name="twitter:title" content="'.$obj->BetterSiteTitle.'">';
         $body = strpos($response->getBody(), $needle);
         $this->assertTrue(is_numeric($body), 'Could not find BetterSiteTitle as twitter:title ');
+    }
+
+    public function testFBDescriptionMetaDescription()
+    {
+        $obj = $this->objFromFixture('Page', 'objectWithMetaDescription');
+        $response = $this->get($obj->Link());
+        $needle = '<meta property="og:description" content="'.$obj->MetaDescription.'"';
+        $body = strpos($response->getBody(), $needle);
+        $this->assertTrue(is_numeric($body), 'Could not find standard MetaDescription as Facebook MetaDescription');
+    }
+
+    public function testTWDescriptionMetaDescription()
+    {
+        $obj = $this->objFromFixture('Page', 'objectWithMetaDescription');
+        $response = $this->get($obj->Link());
+        $needle = '<meta name="twitter:description" content="'.$obj->MetaDescription.'">';
+        $body = strpos($response->getBody(), $needle);
+        $this->assertTrue(is_numeric($body), 'Could not finf standard MetaDescription as Twitter MetaDescription');
     }
 }
