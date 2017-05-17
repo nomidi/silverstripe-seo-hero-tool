177a969b659dffb3200fdbbafa402f7d4b8bfddb
ArminEden <eden@bit-basti.de>
2017-05-17 14:13:33 +0200

    changes to configuration. removed SeoHeroToolLeftAndMain as it is not used at the moment

--- _config.php
+++ _config.php
@@ -3,3 +3,5 @@
 if (!defined('SEO_HERO_TOOL_PATH')) {
     define('SEO_HERO_TOOL_PATH', rtrim(basename(dirname(__FILE__))));
 }
+
+Config::inst()->update('LeftAndMain', 'extra_requirements_css', array(SEO_HERO_TOOL_PATH.'/css/SeoHeroTool.css'));
