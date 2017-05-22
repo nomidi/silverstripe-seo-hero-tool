428897523a5bc72be10d3f71d99ad86f29997d41
ArminEden <eden@bit-basti.de>
2017-05-22 14:50:22 +0200

    added option to ask for advanced settings

--- code/controllers/SeoHeroToolDataObject.php
+++ code/controllers/SeoHeroToolDataObject.php
@@ -209,14 +209,20 @@ class SeoHeroToolDataObject extends DataExtension
 
                 )
             );
+        if (!defined('SEO_HERO_TOOL_ADVANCED_PATH')) {
+            $advancedRemark = _t('SeoHeroTool.AdvancedVersionRemark', 'The Keywords and Keyword Question are just available in the Advanced Version!');
+            $keywordQuestionField->setRightTitle($advancedRemark);
+        }
+
+
         $fields->addFieldToTab('Root.SeoHeroTool', $keywordToggleField);
         $keywordField->setRightTitle(
                 _t('SeoHeroTool.FeaturedKeywordAfter', 'Using commas to separate Keywords..')
             );
-        $keywordQuestionField->setRightTitle(
+    /*    $keywordQuestionField->setRightTitle(
                 _t('SeoHeroTool.KeywordQuestionAfter', 'This field saves questions from the W-Questions, available only in German right now.')
             );
-
+*/
         # translations href
         $langhrefField = "";
         $langhrefFieldLabel = "";
